6e35339f34beb0f4692a2bda5cf697207ec71bdd
Skrider <skrider@gmail.com>
2024-10-13 13:29:51 +0300

    27_2_ne_ege modified

--- other/27_ne_ege_2.py
+++ other/27_ne_ege_2.py
@@ -2,20 +2,33 @@ f = open("file")
 n = int(f.readline())
 a = [int(i) for i in f]
 
-k5 = n5 = 0
+c = [0] * 60
 
 for i in a:
-    if i % 5 == 0:
-        k5 += 1
-n5 = n - k5
-r = (
-    (k5 * (k5 - 1) * (k5 - 2) // 6)
-    + ((k5 * (k5 - 1) // 2) * n5)
-    + ((n5 * (n5 - 1) // 2) * k5)
-)
-print(r)  # Первый вариант (r=r1)
-r1 = (n * (n - 1) * (n - 2) // 6) - ((n5 * (n5 - 1) * (n5 - 2)) // 6)
-print(r1)  # Второй вариант (r=r1)
+    c[i % 60] += 1
+r = sum([i * (i - 1) // 2 for i in c])
+print(r)
+
+
+# f = open("file")
+# n = int(f.readline())
+# a = [int(i) for i in f]
+
+# k5 = n5 = 0
+
+# for i in a:
+#     if i % 5 == 0:
+#         k5 += 1
+# n5 = n - k5
+# r = (
+#     (k5 * (k5 - 1) * (k5 - 2) // 6)
+#     + ((k5 * (k5 - 1) // 2) * n5)
+#     + ((n5 * (n5 - 1) // 2) * k5)
+# )
+# print(r)  # Первый вариант (r=r1)
+# r1 = (n * (n - 1) * (n - 2) // 6) - ((n5 * (n5 - 1) * (n5 - 2)) // 6)
+# print(r1)  # Второй вариант (r=r1)
+
 
 # f = open("file")
 # n = int(f.readline())
